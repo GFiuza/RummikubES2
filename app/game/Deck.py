58810cd7923c87a2b6daf6315bf19007b85b1cda
Gustavo Albino <gustavo.albino.santos@gmail.com>
2019-05-01 10:46:20 -0300

    Separação das classes do jogo

---
+++ app/game/Deck.py
@@ -0,0 +1,45 @@
+import random
+from typing import List
+from app.game.Piece import *
+
+# Classe que representa o 'deck' de peças, quando instanciado ele chama a função build()
+# que popula com todas as 104 peças
+class Deck(object):
+    def __init__(self):
+        self.pieces: List[Piece] = []
+        self.build()
+        self.drawButtnImage = pygame.image.load(os.environ.get("draw_unlock"))  # Caminho da imagem
+        self.drawButtnRect: pygame.Rect = self.drawButtnImage.get_rect()  # 'hitbox' da imagem
+
+    # Popula um deck de 104 pecas
+    def build(self):
+        for color in PieceColor:
+            for value in PieceValue:
+                if value == PieceValue.JOKER or color == PieceColor.JOKER or value == PieceValue.BLANK or color == PieceColor.BLANK:
+                    continue
+                self.pieces.append(Piece(value, color))
+                self.pieces.append(Piece(value, color))
+        self.pieces.append(Piece(PieceValue.JOKER, PieceColor.JOKER))
+        self.pieces.append(Piece(PieceValue.JOKER, PieceColor.JOKER))
+
+    # Realiza um shuffle
+    def shuffle(self):
+        for i in range(len(self.pieces) - 1, 0, -1):
+            r = random.randint(0, i)
+            self.pieces[i], self.pieces[r] = self.pieces[r], self.pieces[i]
+
+    # Função que remove uma peça do deck
+    def draw_piece(self) -> Piece:
+        if not self.is_empty():
+            return self.pieces.pop()
+
+    # Print das peças no terminal
+    def show(self):
+        for p in self.pieces:
+            p.show()
+
+    # Verifica se o deck esta vazio ou não
+    def is_empty(self) -> bool:
+        if len(self.pieces) == 0:
+            return True
+        return False
--- app/game/Table.py
+++ app/game/Table.py
@@ -1,36 +1,8 @@
 import pygame
-from app.game.GameEnums import *
+from app.game.Piece import *
 from typing import List, Tuple
-import random
-import os.path
 
 
-# Classe que representa uma peça do jogo, quando instanciada ela recebe um valor e uma cor,
-# assim como o caminho do seu sprite.
-class Piece(object):
-    def __init__(self, value: PieceValue, color: PieceColor):
-        self.value = value
-        self.color = color
-        self.whereAt = PieceLocale.DECK
-        self.originalPlace = (0, 0)
-        if value != PieceValue.BLANK:
-            if value != PieceValue.JOKER:
-                self.image: pygame.image = pygame.image.load(os.path.join('resources/pieces',
-                                                                          color.name.lower() + "_" + str(
-                                                                              value.value) + ".png"))
-            else:
-                self.image = pygame.image.load(
-                    os.path.join('resources/pieces', color.name.lower() + "_1.png"))
-            self.rect: pygame.Rect = self.image.get_rect()
-
-    def toString(self):
-        return str(self.value.value) + "-" + self.color.value
-
-    def show(self):
-        print("{} - {} ".format(self.color, self.value))
-
-
-# Classe que representa a matriz do tabuleiro, na grid
 class GameTable(object):
     def __init__(self, image_file, location):
         self.tabuleiro: List[List[Piece]] = [[Piece(PieceValue.BLANK, PieceColor.BLANK) for x in range(Table.COLUMNS.value)]
@@ -184,69 +156,3 @@ class GameTable(object):
 
             return relativeX, relativeY
         return -1, -1
-
-
-# Classe que representa o 'deck' de peças, quando instanciado ele chama a função build()
-# que popula com todas as 104 peças
-class Deck(object):
-    def __init__(self):
-        self.pieces: List[Piece] = []
-        self.build()
-        self.drawButtnImage = pygame.image.load(os.environ.get("draw_unlock"))  # Caminho da imagem
-        self.drawButtnRect: pygame.Rect = self.drawButtnImage.get_rect()  # 'hitbox' da imagem
-
-    # Popula um deck de 104 pecas
-    def build(self):
-        for color in PieceColor:
-            for value in PieceValue:
-                if value == PieceValue.JOKER or color == PieceColor.JOKER or value == PieceValue.BLANK or color == PieceColor.BLANK:
-                    continue
-                self.pieces.append(Piece(value, color))
-                self.pieces.append(Piece(value, color))
-        self.pieces.append(Piece(PieceValue.JOKER, PieceColor.JOKER))
-        self.pieces.append(Piece(PieceValue.JOKER, PieceColor.JOKER))
-
-    # Realiza um shuffle
-    def shuffle(self):
-        for i in range(len(self.pieces) - 1, 0, -1):
-            r = random.randint(0, i)
-            self.pieces[i], self.pieces[r] = self.pieces[r], self.pieces[i]
-
-    # Função que remove uma peça do deck
-    def draw_piece(self) -> Piece:
-        if not self.is_empty():
-            return self.pieces.pop()
-
-    # Print das peças no terminal
-    def show(self):
-        for p in self.pieces:
-            p.show()
-
-    # Verifica se o deck esta vazio ou não
-    def is_empty(self) -> bool:
-        if len(self.pieces) == 0:
-            return True
-        return False
-
-
-# Classe que representa um jogador, que possui name, id e uma mão
-class Player(object):
-    def __init__(self, name: str, id: int):
-        self.name: str = name
-        self.hand: List[Piece] = []
-        self.id: int = id
-
-    # Método em que o jogador adiciona à mão dele uma peça do deck
-    def draw(self, deck: Deck):
-        self.hand.append(deck.draw_piece())
-        self.hand[-1].whereAt = PieceLocale.HAND
-        return self
-
-    # Mostra a mão do jogador no terminal
-    def show_hand(self):
-        print("Mao de {}:".format(self.name))
-        for piece in self.hand:
-            piece.show()
-
-    # todo sorting de mão
-    # def sortHand(self):
